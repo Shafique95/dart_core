dad1ecec03537cba5c5be9100c461d584d237138
Abdur Rahman <abdurrahmansoikot123@gmail.com>
2025-07-14 21:53:27 +0600

    Created my own branch and did initial work

---
+++ Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/variable/variable.dart
@@ -0,0 +1,9 @@
+void main(){
+  String name = "Abdur Rahman";
+  int Serial_no = 25;
+  bool joined_first_class = true;
+
+  print("My Name is: $name");
+  print("My Serial Number is: $Serial_no");
+  print("joined_first_class: $joined_first_class");
+}

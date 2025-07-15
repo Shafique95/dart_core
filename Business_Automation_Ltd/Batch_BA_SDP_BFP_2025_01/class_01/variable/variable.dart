bc81394d9686ab6d566a7a24752b081b889fd106
Abdur Rahman <abdurrahmansoikot123@gmail.com>
2025-07-15 23:06:45 +0600

    new added

--- Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/variable/variable.dart
+++ Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/variable/variable.dart
@@ -6,4 +6,33 @@ void main(){
   print("My Name is: $name");
   print("My Serial Number is: $Serial_no");
   print("joined_first_class: $joined_first_class");
+
+  // String in dart
+  String f_name,l_name;
+  f_name = "Flutter";
+  l_name = " Project";
+  var full_name = f_name + l_name;
+  print("Full Project Name is: $full_name");
+  print("\n");
+  
+  // Set in dart
+  Set<String> cities = {"Dhaka","Gazipur","Chandpur","Chattogram"};
+  cities.add("Rangpur");
+  cities.add("Barisal");
+  cities.add("Dhaka");
+
+  print(cities);
+  print("\n");
+  // Map in dart
+  Map<String,String> capitals = {
+    "Bangladesh":"Dhaka",
+    "Pakistan": "Islamabad",
+    "Japan":"Tokyo",
+    "China":"Beijing"
+  };
+
+  capitals.forEach((country, capital) {
+    print('The capital of $country is $capital');
+  });
+
 }

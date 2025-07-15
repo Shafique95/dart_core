a5551975b97a187cd343bdf7eaa4a6e408e15ec2
ErfanulHaque2000 <erfanul.haque2000@gmail.com>
2025-07-16 01:42:04 +0600

    Update conditional.dart

---
+++ Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/condition/conditional.dart
@@ -0,0 +1,15 @@
+void main() {
+  int mark = 60;
+
+  if (mark >= 90) {
+    print("$mark mark receives A grade");
+  } else if (mark >= 80) {
+    print("$mark mark receivess B grade");
+  } else if (mark >= 70) {
+    print("$mark mark receivess C grade");
+  } else if (mark >= 60) {
+    print("$mark mark receivess D grade");
+  } else {
+    print("$mark mark receivess F grade");
+  }
+}

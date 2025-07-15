efcaac5a0d9dde0a829a09ac721d4eafdee40a50
JutonAcharya <121500691+jutonacherjay@users.noreply.github.com>
2025-07-15 14:58:38 +0600

    Add files via upload
    Add examples of variable declaration and data types in Dart

---
+++ Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/basic_variable.dart
@@ -0,0 +1,63 @@
+
+void main() {
+  String name = "Juton";
+  int age = 25;
+  double height = 5.9;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+}
+
+
+
+
+
+
+
+
+
+// // var name = 10;
+
+// // Strongly typed variable
+// // camelCasevar age = 30;
+// // myName() => "John Doe";
+// // country name >>>> // countryName;
+// /// for all class , variables, functions, methods, etc. use camelCase
+
+// void main() {
+//   // int, double, bool, String, List, Map, Set, dynamic
+//   String empName = "John Doe";
+//   empName = "Kamal Hossain"; // Reassigning a new value
+//   print("Hello, $empName");
+//   print(name);
+   
+// }
+
+// // // some keywords in dart are
+// // // abstract, assert, async, await, break, case, catch, class, const, continue, moset popular keywords are
+// // class Person {
+// //   // scope of class variables is within the class
+// //   String? name; // class fields or class vaiables or class properties
+// //   int? age;
+
+// //   Person(name, age){
+// //     // Constructor to initialize the class variables
+// //     print("Person created: $name, Age: $age");
+
+// //   }
+// //   /// Method to display person's information
+// //   void sdfsdfsf() {
+// //     print("Name: $name, Age: $age");
+// //   }
+// // }
+// // // block
+// // // {
+// // // block of code
+
+// // // }
+// // void printMyname() {
+// //   name;
+// // }

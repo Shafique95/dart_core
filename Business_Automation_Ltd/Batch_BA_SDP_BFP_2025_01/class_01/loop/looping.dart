98ad980b86a298a0b39c3cc91c68e8ee61385dc9
JutonAcharya <121500691+jutonacherjay@users.noreply.github.com>
2025-07-15 17:08:49 +0600

    Add files via upload
    Practice loops in Dart: for, while

---
+++ Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/loop/looping.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("For loop:");
+  for (int i = 1; i <= 5; i++) {
+    print("Count: $i");
+  }
+
+  print("While loop:");
+  int j = 1;
+  while (j <= 5) {
+    print("Count: $j");
+    j++;
+  }
+}

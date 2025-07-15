e3eb63b06862f3ff7d81f68c26d98cc6b239dbdd
JutonAcharya <121500691+jutonacherjay@users.noreply.github.com>
2025-07-15 15:26:32 +0600

    Add files via upload
    Add Dart functions with parameters, return types, and void functions

--- Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/function/function.dart
+++ Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/function/function.dart
@@ -1,3 +1,15 @@
-int returnAvalue() {
-  return 42;
+
+int add(int a, int b) {
+  return a + b;
+}
+
+void greet(String name) {
+  print("Hello, $name!");
+}
+
+void main() {
+  int result = add(10, 20);
+  print("Sum: $result");
+
+  greet("Juton");
 }

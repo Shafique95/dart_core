ae2131121eeb8ab0066398f196d06f2c0cd4919b
ErfanulHaque2000 <erfanul.haque2000@gmail.com>
2025-07-16 02:04:19 +0600

    Update function.dart

--- Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/function/function.dart
+++ Business_Automation_Ltd/Batch_BA_SDP_BFP_2025_01/class_01/function/function.dart
@@ -1,3 +1,22 @@
-int returnAvalue() {
-  return 42;
+void grade(int, mark) {
+
+  if (mark >= 90) {
+    print("$mark mark receives A grade");
+  } else if (mark >= 80) {
+    print("$mark mark receivess B grade");
+  } else if (mark >= 70) {
+    print("$mark mark receivess C grade");
+  } else if (mark >= 60) {
+    print("$mark mark receivess D grade");
+  } else {
+    print("$mark mark receivess F grade");
+  }
+}
+
+void main() {
+  mark(92);
+  mark(61);
+  mark(74);
+  mark(32);
+  mark(87);
 }
